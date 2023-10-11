fe4549a0dba144c5b1bf06f313aca3e44a94842e
jRydell <johannilsrydell89@gmail.com>
2023-10-11 16:26:30 +0200

    eventlistners

--- javascript.js
+++ javascript.js
@@ -1,20 +1,25 @@
-
-
-
-//Array för hemliga ord
-const secretWords = ['apa', 'tjernobyl', 'hus', 'hund', 'katt', 'hiss', 'banan', 'bil', 'lägenhet', 'skrivbord']
-
-//Slumpa fram ord ur array
-let secretWord= secretWords[Math.floor(Math.random() * secretWords.length)]
-
+//Array med ord
+const words = ['apa', 'tjernobyl', 'hus', 'hund', 'katt', 'hiss', 'banan', 'bil', 'lägenhet', 'skrivbord'];
 //Array för gissade bokstäver
-const guessedLetters = []
+let guessedLetters = []; 
 
 
+//Variabel för slumpat ord och försöks räknare
+let randomWord = ''; 
+
+let remainingGuesses = 6; 
 
 
-//Event listner för att trycka ned en knapp på tangentbordet
+//Event listners:
 
-document.addEventListener('keydown', function(event) {
-    const guessedLetter = event.key.toLowerCase();
-});
+//Starta spel
+const startButton = document.querySelector('#startButton');
+startButton.addEventListener('click', startGame); // Fixa funktion för att starta/Starta om spelet.
+
+//Gissa knapp
+const guessButton = document.querySelector('#guessButton');
+guessButton.addEventListener('click', guessLetter); //Fixa en funktion för att ta in gissning.
+
+// Funktioner:
+
+
