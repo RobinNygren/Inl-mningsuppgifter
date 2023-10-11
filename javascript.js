06f3589a57fea1d8fcec1d873b8ae2a86a7b4a4a
jRydell <johannilsrydell89@gmail.com>
2023-10-11 09:20:35 +0200

    sista hemma

--- javascript.js
+++ javascript.js
@@ -1,15 +1,21 @@
 
 
 
-
+//Array för hemliga ord
+const secretWords = ['apa', 'tjernobyl', 'hus', 'hund', 'katt', 'hiss', 'banan', 'bil', 'lägenhet', 'skrivbord']
 
 //Slumpa fram ord ur array
-const secretWords = ['apa', 'tjernobyl', 'hus'] 
 let secretWord= secretWords[Math.floor(Math.random() * secretWords.length)]
+
+//Array för gissade bokstäver
+const guessedLetters = []
+
+//Array för rätt gissade bokstäver
+const correctLetters = []
 
 
 //Event listner för att trycka ned en knapp på tangentbordet
-const guessedLetters = [];
+
 document.addEventListener('keydown', function(event) {
     const guessedLetter = event.key.toLowerCase();
 });
